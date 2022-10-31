9620f9dd001ee594892abc241a105cb1134b7f78
Muhammed Dilmaç <73828688+muhammeddilmacc@users.noreply.github.com>
2022-10-31 22:04:55 +0300

    create task model and get all tasks

--- starter/models/Task.js
+++ starter/models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
 
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],                      // this is a custom error message
+        trim: true,                                                     // remove white space
+        maxlength: [20, 'Name cannot be more than 20 characters']       // max length of name
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 
 });
 
